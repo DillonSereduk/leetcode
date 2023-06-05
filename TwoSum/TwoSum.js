03bf67a175e49ae34c29be31d65f8fa17fdd90f2
Dillon Sereduk <102322087+DillonSereduk@users.noreply.github.com>
2023-06-04 21:27:36 -0700

    Create TwoSum.js

---
+++ TwoSum/TwoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) { 
+    for(var i = 0; i < nums.length; i++) { // For as long as i is less than nums length, increment i by 1
+        for(var j = i + 1; j < nums.length; j++) { // For as long as j is less than nums.length, increment j by 1
+            if(nums[i] + nums[j] === target) {
+                return [i,j];
+            }
+        }
+    }
+};
