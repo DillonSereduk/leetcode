ba560f541a30c0a71cf23c5448aa393b8bdfdaf0
Dillon Sereduk <102322087+DillonSereduk@users.noreply.github.com>
2023-06-09 21:19:12 -0700

    Suedo Code Update RomanToInteger.js
    For future reference and studying!

--- RomanToInteger/RomanToInteger.js
+++ RomanToInteger/RomanToInteger.js
@@ -4,7 +4,7 @@
  */
 
 var romanToInt = function(s) {
-    const symbols = {
+    const symbols = { // Map between the symbols and integers
         'I' : 1,
         'V' : 5,
         'X' : 10,
@@ -14,16 +14,16 @@ var romanToInt = function(s) {
         'M' : 1000
     }
     
-    let total = 0;
+    let total = 0; // Defining total
 
-    for(let i = 0; i < s.length; i++) {
-        let current = s[i];
-        let next = s[i + 1];
+    for(let i = 0; i < s.length; i++) { // For as long as 'i' is less than 's.length', increment by one
+        let current = s[i]; // focus point
+        let next = s[i + 1]; // advancing the focus point by one index
 
-        if(symbols[current] < symbols[next]) {
-            total -= symbols[current];
-        } else {
-            total += symbols[current];
+        if(symbols[current] < symbols[next]) { // If the current value is less than the next value...
+            total -= symbols[current]; // ...subtract the current value from the total
+        } else { // If the current value is greater than the next value...
+            total += symbols[current]; // ...add the current value to the total
         }
     }
 
