396893ff30d655bb7e95ce660fafae844f1a3434
Dillon Sereduk <102322087+DillonSereduk@users.noreply.github.com>
2023-06-23 16:54:59 -0700

    Create AddTwoIntegers.js

---
+++ AddTwoIntegers/AddTwoIntegers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    return num1 + num2;
+};
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+// Super simple
