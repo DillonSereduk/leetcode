980d82b6f922dedf00d397c9624892d551fae675
Dillon Sereduk <102322087+DillonSereduk@users.noreply.github.com>
2023-06-23 20:54:19 -0700

    Update RootEqualsSumOfChildren.js

--- RootEqualsSumOfChildren/RootEqualsSumOfChildren.js
+++ RootEqualsSumOfChildren/RootEqualsSumOfChildren.js
@@ -1,7 +1,11 @@
 const checkTree = root => root.val === (root.left.val + root.right.val);
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 
 // OR 
 
 var checkTree = function(root) {
   return root.val === root.left.val + root.right.val;
 };
+
+
